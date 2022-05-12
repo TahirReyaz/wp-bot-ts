e9a620a03badc8dd5784b3f6f9d99af92de1c0cc
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-05-12 13:55:01 +0530

    Trying non modular version

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-const { default: venom, Whatsapp, Message } = require("venom-bot");
-
-const { sendReply } = require("./helpers/venomFunctions");
-
+// Supports ES6
+const venom = require("venom-bot");
+const { Whatsapp } = require("venom-bot");
+// import { sendReply } from "./helpers/venomFunctions";
 // Create the client
 venom
   .create({
     session: "session-name", //name of session
     multidevice: true, // for version not multidevice use false.(default: true)
   })
-  .then((client: typeof Whatsapp) => {
+  .then((client: any) => {
     start(client);
   })
   .catch((error: any) => {
@@ -17,32 +17,47 @@ venom
   });
 let RecievedMsgPermission = false;
 // Start the client
-function start(client: typeof Whatsapp) {
+function start(client: any) {
   // This function executes whenever a message is sent or recieved
-  client.onAnyMessage((message: typeof Message) => {
-    // variables and constants required to make the data readable
-    const data = message.body;
-    const botQuery = data.split(" ");
-    botQuery[0] = botQuery[0].toLowerCase();
-    const queryCutter = botQuery[0] + " ";
-    const queryWithDesc = data.substring(queryCutter.length).split("\n"); // Get everything written after the command
-    let query = queryWithDesc[0]; // This is used as the option people type after the command
-    const queryPart = query.split("-"); // This is used as extra options that people type after the above query
+  client.onAnyMessage(
+    (message: { body: any; chatId: any; id: { toString: () => any } }) => {
+      // variables and constants required to make the data readable
+      const data = message.body;
+      const botQuery = data.split(" ");
+      botQuery[0] = botQuery[0].toLowerCase();
+      const queryCutter = botQuery[0] + " ";
+      const queryWithDesc = data.substring(queryCutter.length).split("\n"); // Get everything written after the command
+      let query = queryWithDesc[0]; // This is used as the option people type after the command
+      const queryPart = query.split("-"); // This is used as extra options that people type after the above query
 
-    switch (botQuery[0]) {
-      //////////////////////////////////////HI BOT//////////////////////////////////////
-      case ".hi":
-      case "hibot":
-        RecievedMsgPermission = true;
-        console.log("in hi");
-        sendReply(
-          client,
-          message.chatId,
-          "No need to say hi to me, I am always here, reading every message you send to this guy.😁\nSend 'HelpBot' for commands",
-          message.id.toString(),
-          "Error when sending: "
-        );
-        break;
+      switch (botQuery[0]) {
+        //////////////////////////////////////HI BOT//////////////////////////////////////
+        case ".hi":
+        case "hibot":
+          RecievedMsgPermission = true;
+          console.log("in hi");
+          const msg =
+            "No need to say hi to me, I am always here, reading every message you send to this guy.😁\nSend 'HelpBot' for commands";
+
+          client
+            .reply(message.chatId, msg, message.id.toString())
+            .then(() => {
+              console.log(
+                "Reply sent:\n" + msg + "\n------------------------------"
+              );
+            })
+            .catch((erro: any) => {
+              console.error(erro);
+            });
+          // sendReply(
+          //   client,
+          //   message.chatId,
+          //   "No need to say hi to me, I am always here, reading every message you send to this guy.😁\nSend 'HelpBot' for commands",
+          //   message.id.toString(),
+          //   "Error when sending: "
+          // );
+          break;
+      }
     }
-  });
+  );
 }
